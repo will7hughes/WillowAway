a8a56dce70aa0dc32d704e745ea25ff27151ea94
Willow <will7hughes@gmail.com>
2023-03-21 21:29:37 -0500

    Removed styles.css from layout

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<WillowAwayContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WillowAwayContext")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
