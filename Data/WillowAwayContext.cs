91af2f290100f88978ef9cd5a9533b291f704436
Willow <will7hughes@gmail.com>
2023-03-22 09:43:41 -0500

    Created User model with scaffold item. Connecting to MS SQL database

---
+++ Data/WillowAwayContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WillowAway.Models;
+
+namespace WillowAway.Data
+{
+    public class WillowAwayContext : DbContext
+    {
+        public WillowAwayContext (DbContextOptions<WillowAwayContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WillowAway.Models.User> User { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WillowAway.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WillowAwayContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WillowAwayContext") ?? throw new InvalidOperationException("Connection string 'WillowAwayContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
